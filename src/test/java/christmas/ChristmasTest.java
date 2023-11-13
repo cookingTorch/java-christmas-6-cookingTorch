d88b6c7331a88f21eb4a8b919e94178646b2b5e0
cookingTorch <jhw7181@naver.com>
2023-11-14 00:03:26 +0900

    test(ChristmasTest): add validation tests for menu input
    Add tests in Validator for menu input validation. Check for errors when
    ordering items not on the menu, entering quantities less than one,
    submitting orders in an incorrect format, and placing duplicate items
    in the order.

--- src/test/java/christmas/ChristmasTest.java
+++ src/test/java/christmas/ChristmasTest.java
@@ -30,4 +30,36 @@ public class ChristmasTest {
         assertThatThrownBy(() -> validator.validateDate("32"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("메뉴판에 없는 메뉴를 입력하는 경우 에러가 발생한다.")
+    @Test
+    void orderNotInMenu() {
+        Validator validator = new Validator();
+        assertThatThrownBy(() -> validator.validateMenu("타파스-1,포터하우스-2,바비큐립-1"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("메뉴의 개수가 1보다 작으면 에러가 발생한다.")
+    @Test
+    void orderLowerOne() {
+        Validator validator = new Validator();
+        assertThatThrownBy(() -> validator.validateMenu("타파스-1,티본스테이크-0,바비큐립-1"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("메뉴 형식이 맞지 않으면 에러가 발생한다.")
+    @Test
+    void orderInvalidForm() {
+        Validator validator = new Validator();
+        assertThatThrownBy(() -> validator.validateMenu("타파스-1,티본스테이크--2,바비큐립-1"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("중복 메뉴가 있으면 에러가 발생한다.")
+    @Test
+    void orderDuplicateMenu() {
+        Validator validator = new Validator();
+        assertThatThrownBy(() -> validator.validateMenu("티본스테이크-1,티본스테이크-2,바비큐립-1"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
